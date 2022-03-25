34e99856e31ae3e4f881987655a6f6230a7a0992
Kelvin <58968680+kae1213@users.noreply.github.com>
2022-03-24 18:06:08 -0700

    UI for Swift

---
+++ comp380SecondProj/comp380SecondProj/BookFlightTab.swift
@@ -0,0 +1,30 @@
+//
+//  BookFlightTab.swift
+//  comp380SecondProj
+//
+//  Created by Kelvin M on 3/24/22.
+//
+
+import UIKit
+
+class BookFlightTab: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tabBarItem.image = UIImage(named: "plane")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ comp380SecondProj/comp380SecondProj/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  comp380SecondProj
+//
+//  Created by Kelvin M on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+    
+
+    
+}
+
