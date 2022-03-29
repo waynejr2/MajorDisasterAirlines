f244bdbe464d9c573e9ac6a15f084b5c33b01e61
Kelvin <58968680+kae1213@users.noreply.github.com>
2022-03-29 15:13:55 -0700

    Added Interface to Start Reservation

---
+++ src/createReservation.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class createReservation {
+    private JPanel panel1;
+    private JButton backButton;
+    private JTextField fromEntry;
+    private JLabel FromButton;
+    private JLabel selectionTitle;
+    private JLabel AvailReservation;
+}
