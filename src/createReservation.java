3e6e5d626a4557501c79f36dcd4322082752e7a8
Kelvin <58968680+kae1213@users.noreply.github.com>
2022-03-30 12:54:05 -0700

    Create Reservation Form Interface Complete -Logic Code Needed

--- src/createReservation.java
+++ src/createReservation.java
@@ -2,9 +2,16 @@ import javax.swing.*;
 
 public class createReservation {
     private JPanel panel1;
-    private JButton backButton;
     private JTextField fromEntry;
-    private JLabel FromButton;
+    private JLabel FromLabel;
     private JLabel selectionTitle;
     private JLabel AvailReservation;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton searchButton;
+    private JButton backButton;
+    private JButton selectButton;
+    private JLabel ToLabel;
+    private JLabel DepartureDateLabel;
+    private JScrollPane availabilityPane;
 }
