6fc27758059c00b3e6e5e15d3f00c8969aab302a
Kelvin <58968680+kae1213@users.noreply.github.com>
2022-03-30 14:33:02 -0700

    Added Main Menu Buttons

---
+++ src/mainMenuChoices.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class mainMenuChoices {
+    private JPanel panel1;
+    private JButton searchFlightsButton;
+    private JButton createReservationButton;
+    private JButton viewReservationButton;
+    private JButton editReservationButton;
+}
