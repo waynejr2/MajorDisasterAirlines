e9d09a02c72b152eefe36c7d26d542f7ef9aa6d3
wayne rasmussen <waynejrasmussen@gmail.com>
2022-04-01 12:18:48 -0700

    Added example java code for db

---
+++ src/DatabaseExample.java
@@ -0,0 +1,9 @@
+public class DatabaseExample {
+
+        public static void main(String[] args) {
+            MainFrame mf = new MainFrame();
+            System.out.println("Howdy World, Java app");
+        }
+
+
+}
