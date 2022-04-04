64d8433f6ae05cd54d04e6087f13386726643fe3
Kelvin <58968680+kae1213@users.noreply.github.com>
2022-04-04 11:40:38 -0700

    Edit Reservation UI Done

---
+++ src/Objects/editReservation.java
@@ -0,0 +1,12 @@
+package Objects;
+
+import javax.swing.*;
+
+public class editReservation {
+    private JButton cancelButton;
+    private JButton enterButton;
+    private JTextField enterBirthInput;
+    private JTextField enterConfirmInput;
+    private JLabel enterConfirmLabel;
+    private JLabel enterBirthLabel;
+}
