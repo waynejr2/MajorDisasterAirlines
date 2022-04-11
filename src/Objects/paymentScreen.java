6feb96acdfa259a5b8cba030d47e3d33730b9e13
Juanxsb <78043641+Juanxsb@users.noreply.github.com>
2022-04-11 13:42:00 -0700

    Payment Screen Added still working on it

---
+++ src/Objects/paymentScreen.java
@@ -0,0 +1,11 @@
+package Objects;
+
+import javax.swing.*;
+
+public class paymentScreen {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton confirmButton;
+    private JButton cancelButton;
+}
