5d6b0079536d714f9943012b1933e8e6441eaf21
Juanxsb <78043641+Juanxsb@users.noreply.github.com>
2022-04-16 17:10:27 -0700

    worked on invalid card for confirmedFlight as  well as after  confirmedFlight sending back to main menu. also paymentScreen

--- src/Objects/confirmedFlight.java
+++ src/Objects/confirmedFlight.java
@@ -1,7 +1,51 @@
 package Objects;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class confirmedFlight {
-    private JButton button1;
+public class confirmedFlight extends JFrame{
+    private JButton doneButton;
+    private JLabel bookingConLabel;
+    private JLabel bookingDateLabel;
+    private JLabel guestLabel;
+    private JLabel flightDetailsLabel;
+    private JLabel thankYouLabel;
+    private JPanel confirmedFlightPanel;
+
+    private mainMenuChoices mainMenuChoicesWindow;
+
+    public confirmedFlight(mainMenuChoices mainMenuChoicesWindow){
+
+        this.mainMenuChoicesWindow = mainMenuChoicesWindow;
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        int windowHeight = 600;
+        int windowWidth = 1000;
+
+        confirmedFlightPanel.setLayout(new GridLayout(20, 1, 2, 5));
+        setContentPane(confirmedFlightPanel);
+        setTitle("Choose");
+        setSize(windowWidth, windowHeight);
+        setLocation(screenWidth/2 - windowWidth/2, screenHeight/2 - windowHeight/2 - 50);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        doneButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mainMenuChoicesWindow.activate();
+                deactivate();
+            }
+        });
+    }
+    public void activate() {
+        setVisible(true);
+    }
+    public void deactivate() {
+        setVisible(false);
+    }
 }
+
--- src/Objects/paymentScreen.java
+++ src/Objects/paymentScreen.java
@@ -1,12 +1,65 @@
 package Objects;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class paymentScreen {
+public class paymentScreen extends JFrame{
     private JTextField ownerText;
     private JTextField cvvText;
     private JTextField exText;
     private JButton confirmButton;
     private JButton cancelButton;
     private JTextField cardText;
+    private JLabel InvalidCard;
+    private JPanel paymentMethodPanel;
+    private JLabel ownerLabel;
+    private JLabel paymentMethod;
+    private JLabel experationDateLabel;
+    private JLabel cvvLabel;
+    private JLabel cardNumberLabel;
+
+    private final addAdditionalyBaggage addAdditionalyBaggagePanel;
+
+    public paymentScreen(addAdditionalyBaggage addAdditionalyBaggagePanel){
+
+        this.addAdditionalyBaggagePanel = addAdditionalyBaggagePanel;
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        int windowHeight = 600;
+        int windowWidth = 1000;
+
+        InvalidCard.setVisible(false);
+
+        paymentMethodPanel.setLayout(new GridLayout(20, 1, 2, 5));
+        setContentPane(paymentMethodPanel);
+        setTitle("Choose");
+        setSize(windowWidth, windowHeight);
+        setLocation(screenWidth/2 - windowWidth/2, screenHeight/2 - windowHeight/2 - 50);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+
+        confirmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String cardNumberLabelText = cardNumberLabel.getText();
+                String cvvLabelText = cvvLabel.getText();
+                    InvalidCard.setVisible(false);
+            }
+
+        });
+
+    }
+
+    public void activate() {setVisible(true);}
+    public void deactivate() {setVisible(false);}
 }
