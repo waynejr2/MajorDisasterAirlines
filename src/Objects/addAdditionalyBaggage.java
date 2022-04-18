2fc083c68a1d16744c2edfc57c173a84322e0ada
Juanxsb <78043641+Juanxsb@users.noreply.github.com>
2022-04-18 13:56:27 -0700

    add additional baggage and payment screen now combined

--- src/Objects/addAdditionalyBaggage.java
+++ src/Objects/addAdditionalyBaggage.java
@@ -6,4 +6,11 @@ public class addAdditionalyBaggage extends JFrame {
     private JPanel addAdditonalBaggagePanel;
     private JTextField textField1;
     private JTextField textField2;
+
+    public void activate() {
+        setVisible(true);
+    }
+    public void deactivate() {
+        setVisible(false);
+    }
 }
